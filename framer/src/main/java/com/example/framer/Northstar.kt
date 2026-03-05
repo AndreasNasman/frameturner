4eb4d1d54e14c9676e4bf1c0ca50b513a21968af
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2026-03-05 12:53:19 +0200

    Add Compose

--- framer/src/main/java/com/example/framer/Northstar.kt
+++ framer/src/main/java/com/example/framer/Northstar.kt
@@ -1,5 +1,14 @@
 package com.example.framer
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
 class Positioning {
-    val name = "Walkbase Northstar"
+    var name by mutableStateOf("Walkbase Northstar")
+
+    fun changeName(newName: String) {
+        println("Changing name from $name to $newName")
+        name = newName
+    }
 }
